54d48b37e295fa878e32f53165188ffc2638581d
angelaserantes <aserantesreboreda@danielcastelao.org>
2022-10-24 11:01:16 +0200

    ejercicio 5

---
+++ src/com/ejercios/ejercicios_semana_quinta/ejercicio5.java
@@ -0,0 +1,41 @@
+package com.ejercios.ejercicios_semana_quinta;
+
+import java.util.Scanner;
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+        Scanner datos = new Scanner(System.in);
+        System.out.println("Introduce un número");
+        int n1 = datos.nextInt();
+        System.out.println("Introduce otro número");
+        int n2 = datos.nextInt();
+        System.out.println("Introduce otro número");
+        int n3 = datos.nextInt();
+        System.out.println("Introduce otro número");
+        int n4 = datos.nextInt();
+        System.out.println("Introduce otro número");
+        int n5 = datos.nextInt();
+        System.out.println("Introduce otro número");
+        int n6 = datos.nextInt();
+        System.out.println("Introduce otro número");
+        int n7 = datos.nextInt();
+        System.out.println("Introduce otro número");
+        int n8 = datos.nextInt();
+        System.out.println("Introduce otro número");
+        int n9 = datos.nextInt();
+        System.out.println("Introduce otro número");
+        int n10 = datos.nextInt();
+        System.out.println("La media aritmética es " + (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10) /10);
+    }
+}
+
+
+
+
+
+
+/*
+Crea un programa que calcule la media acumulada de 10 números que
+introduzca el usuario y muestre por pantalla cual es la media en cada
+momento.
+*/
